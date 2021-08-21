caa65a39931614028c9a817449d903cfc689f3e4
mne17let <vladimirsemovskih@gmail.com>
2021-08-21 23:04:32 +0400

    Работа с EditText

--- app/src/main/java/com/example/studyapp/MainActivity.kt
+++ app/src/main/java/com/example/studyapp/MainActivity.kt
@@ -7,14 +7,13 @@ import android.graphics.drawable.Drawable
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.SpannableString
-import android.text.Spanned
-import android.text.TextPaint
+import android.text.*
 import android.text.method.LinkMovementMethod
 import android.text.style.ClickableSpan
+import android.util.Patterns.EMAIL_ADDRESS
 import android.view.View
-import android.widget.ImageView
-import android.widget.TextView
+import android.view.inputmethod.InputMethodManager
+import android.widget.*
 import androidx.annotation.ColorRes
 import androidx.annotation.StringRes
 import androidx.core.content.res.ResourcesCompat
@@ -22,93 +21,95 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.RequestBuilder
 import com.bumptech.glide.RequestManager
 import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
 
-    private companion object{
-        //const val URL_FOR_DOWNLOAD_IMAGE = "https://zavistnik.com/wp-content/uploads/2020/03/Android-kursy-zastavka.jpg"
-        const val URL_FOR_DOWNLOAD_IMAGE = "https://images7.alphacoders.com/109/1092420.jpg"
-        const val url_new = "https://images4.alphacoders.com/109/1095230.jpg"
-    }
-
-    private lateinit var textView: TextView
-    private lateinit var imageView: ImageView
+    private lateinit var inputLayout: TextInputLayout
+    private lateinit var editText: TextInputEditText
+    private lateinit var buttonCheckEmail: Button
+    private lateinit var checkBox: CheckBox
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        initImageView()
-        downloadImageByGlide()
-
+        init()
+        setEditText()
+        setButton()
+        setCheckbox()
     }
 
-    fun initImageView(){
-        imageView = findViewById(R.id.id_imageview_icon)
-        imageView.setImageResource(R.drawable.original)
+    fun init(){
+        inputLayout = findViewById(R.id.id_input_layout)
+        editText = inputLayout.editText as TextInputEditText
+        buttonCheckEmail = findViewById(R.id.id_button_check_email)
+        checkBox = findViewById(R.id.id_checkbox)
+        buttonCheckEmail.isEnabled = false
     }
 
-    fun downloadImageByGlide(){
-        val requestManager: RequestManager = Glide.with(this)
-        val loadResult: RequestBuilder<Drawable> = requestManager.load(URL_FOR_DOWNLOAD_IMAGE)
-        val resultToRound = loadResult.circleCrop()
-        val holder = ResourcesCompat.getDrawable(resources, android.R.drawable.ic_media_pause, null)
-        if (holder != null) {
-            holder.setTint(resources.getColor(R.color.redColorTitle))
-        }
-        //val addPlaceholder = resultToRound.placeholder(ResourcesCompat.getDrawable(resources, android.R.drawable.ic_media_pause, null))
-        val addPlaceholder = resultToRound.placeholder(holder)
-        val addError = addPlaceholder.error(ResourcesCompat.getDrawable(resources, android.R.drawable.ic_dialog_alert, null))
-        addError.into(imageView)
-    }
+    fun setEditText(){
+        /*editText.addTextChangedListener(object : AbstractTextWatcher(){
+            override fun afterTextChanged(s: Editable?) {
+                if (s.toString().endsWith("@g")){
+                    s?.append("mail.com")
+                }
 
-    /*fun downloadImageByPicasso(){
-        val requestCreator: RequestCreator = Picasso.get().load(URL_FOR_DOWNLOAD_IMAGE)
-        requestCreator.centerCrop()
-        requestCreator.resize(720, 1080)
-        requestCreator.placeholder(android.R.drawable.ic_media_pause)
-        requestCreator.error(android.R.drawable.ic_dialog_alert)
-
-        requestCreator.into(imageView)
-    }*/
-
-    fun downloadImage(){
-        val newThread = createAndGetNewThread()
-        newThread.start()
-    }
-
-    fun createAndGetNewThread(): ThreadForDownloadImage{
-        val callBackForDownloadThread = getNewCallBackForDownloadImage()
-        val newThreadForDownloadImage = ThreadForDownloadImage(URL_FOR_DOWNLOAD_IMAGE, callBackForDownloadThread)
-        return newThreadForDownloadImage
-    }
-
-    fun getNewCallBackForDownloadImage(): DownloadImageCallback{
-
-        class myRunnableForSetImage(private val bitmap: Bitmap) : Runnable{
-            override fun run() {
-                imageView.setImageBitmap(bitmap)
+                val valid = android.util.Patterns.EMAIL_ADDRESS.matcher(s.toString()).matches()
+                inputLayout.isErrorEnabled = !valid
+                val errorText = if (valid) "" else "Введите корректный почтовый адрес"
+                inputLayout.error = errorText
+                if (valid){
+                 Toast.makeText(this@MainActivity, "Почта введена верно", Toast.LENGTH_SHORT).show()
+                }
             }
-        }
+        })*/
 
-        class myRunnableForShowError() : Runnable{
-            override fun run() {
-                Snackbar.make(imageView, "Ошибка загрузки", Snackbar.LENGTH_SHORT).show()
-            }
-        }
-
-        val newCallBack = object : DownloadImageCallback{
-            override fun success(bitmap: Bitmap) {
-                runOnUiThread(myRunnableForSetImage(bitmap))
-
+        editText.listenTextChanges {
+            textEditableType ->
+            if (textEditableType.toString().endsWith("@g")){
+                textEditableType?.append("mail.com")
             }
 
-            override fun failed() {
-                runOnUiThread(myRunnableForShowError())
+            val valid = android.util.Patterns.EMAIL_ADDRESS.matcher(textEditableType.toString()).matches()
+            inputLayout.isErrorEnabled = !valid
+            val errorText = if (valid) "" else "Введите корректный почтовый адрес"
+            inputLayout.error = errorText
+            if (valid){
+                Toast.makeText(this@MainActivity, "Почта введена верно", Toast.LENGTH_SHORT).show()
             }
         }
-
-        return newCallBack
     }
+
+    fun setButton(){
+        buttonCheckEmail.setOnClickListener{
+            if (EMAIL_ADDRESS.matcher(editText.text.toString()).matches()){
+                Snackbar.make(buttonCheckEmail, "Успешный вход", Snackbar.LENGTH_SHORT).show()
+            } else {
+                inputLayout.isErrorEnabled = true
+                inputLayout.error = "Некорректная почта"
+            }
+
+            val imm: InputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(buttonCheckEmail.windowToken, 0)
+
+        }
+    }
+
+    fun setCheckbox(){
+        checkBox.setOnCheckedChangeListener{ first, isChecked ->
+            buttonCheckEmail.isEnabled = isChecked
+        }
+    }
+}
+
+
+fun TextInputEditText.listenTextChanges(block: (text: Editable?) -> Unit){
+    this.addTextChangedListener(object : AbstractTextWatcher(){
+        override fun afterTextChanged(s: Editable?) {
+            block.invoke(s)
+        }
+    })
 }
 
